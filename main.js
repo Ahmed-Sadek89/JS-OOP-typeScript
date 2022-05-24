c7f49492e3fb2ff4dfdbfc944ceab9a5acb59752
Ahmed-Sadek <ahmeamohamed161@gmail.com>
2022-05-24 21:48:01 +0200

    constructor function

---
+++ main.js
@@ -0,0 +1,13 @@
+function User(name, age){
+    this.name = name;
+    this.age = age
+} 
+
+const user1 = new User('ahmed', 25)
+console.log({...user1});
+
+const user2 = new User('amr', 24)
+console.log({...user2});
+
+const user3 = new User('aya', 18)
+console.log({...user3});
